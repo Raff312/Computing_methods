2434a9674d14b109599bb6546ef81a040481efaf
Rafik <bikmaevrafik@gmail.com>
2021-09-07 23:10:18 +0300

    Init

---
+++ Lab1(Gauss)/Lab1(Gauss)/Matrix.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Globalization;
+
+namespace Lab1_Gauss_ {
+    public class Matrix {
+        private readonly double[,] _data;
+
+        public int RowsNum => _data.GetLength(0);
+        public int ColsNum => _data.GetLength(1);
+
+        public double this[int i, int j] {
+            get => _data.GetValue(i, j) == null ? 0 : Convert.ToDouble(_data.GetValue(i, j));
+            set => _data.SetValue(value, i, j);
+        }
+
+        public Matrix(int n, int m) {
+            _data = new double[n, m];
+            Fill(0);
+        }
+
+        public Matrix(double[,] data) {
+            var rows = data.GetLength(0);
+            var cols= data.GetLength(1);
+
+            _data = new double[rows, cols];
+            for (var i = 0; i < rows; i++) {
+                for (var j = 0; j < cols; j++) {
+                    _data[i, j] = data[i, j];
+                }
+            }
+        }
+
+        public void SwapRows(int row1, int row2) {
+            if (row1 < 0 || row1 > ColsNum || row2 < 0 || row2 > ColsNum) {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            if (row1 == row2) return;
+
+            for (var i = 0; i < ColsNum; i++) {
+                var temp = _data[row1, i];
+                _data[row1, i] = _data[row2, i];
+                _data[row2, i] = temp;
+            }
+        }
+
+        public string ToString(bool format = false) {
+            var result = "";
+            for (var i = 0; i < RowsNum; i++) {
+                for (var j = 0; j < ColsNum; j++) {
+                    var value = format ? _data[i, j].ToString("0.##") : _data[i, j].ToString(CultureInfo.CurrentCulture);
+                    result += value + "\t";
+                }
+
+                result += "\n";
+            }
+
+            return result;
+        }
+
+        private void Fill(double x) {
+            for (var i = 0; i < RowsNum; i++) {
+                for (var j = 0; j < ColsNum; j++) {
+                    _data[i, j] = x;
+                }
+            }
+        }
+    }
+}
---
+++ Lab1(Gauss)/Lab1(Gauss)/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+
+namespace Lab1_Gauss_ {
+    internal class Program {
+        private static void Main(string[] args) {
+            try {
+                var data = new double[,] {
+                    {2, 3, -1, 7},
+                    {1, -1, 6, 14},
+                    {6, -2, 1, 11}
+                };
+
+                if (data.GetLength(0) != data.GetLength(1) - 1) {
+                    throw new Exception("Matrix is not square");
+                }
+
+                var matrix = new Matrix(data);
+
+                Console.WriteLine("Initial matrix: ");
+                Console.WriteLine(matrix.ToString());
+
+                TriangleMatrix(matrix);
+                Console.WriteLine("Triangle matrix: ");
+                Console.WriteLine(matrix.ToString(true));
+
+                var list = Foo(matrix);
+                Console.WriteLine("Result: ");
+                ShowResult(list, true);
+            } catch (Exception e) {
+                Console.WriteLine(e);
+            }
+        }
+
+        private static void TriangleMatrix(Matrix matrix) {
+            for (var j = 0; j < matrix.ColsNum - 2; j++) {
+                Rearrange(matrix, j);
+
+                for (var i = j + 1; i < matrix.RowsNum; i++) {
+                    if (matrix[j, j] == 0) continue;
+                    var alpha = -matrix[i, j] / matrix[j, j];
+                    CombineRows(matrix, j, i, alpha);
+                }
+            }
+        }
+
+        private static void Rearrange(Matrix matrix, int pivotIndex) {
+            var maxValue = matrix[pivotIndex, pivotIndex];
+            var newPivotIndex = pivotIndex;
+
+            for (var i = pivotIndex; i < matrix.RowsNum; i++) {
+                if (!(matrix[i, pivotIndex] > maxValue)) continue;
+                maxValue = matrix[i, pivotIndex];
+                newPivotIndex = i;
+            }
+
+            if (newPivotIndex != pivotIndex) {
+                matrix.SwapRows(pivotIndex, newPivotIndex);
+            }
+        }
+
+        private static void CombineRows(Matrix matrix, int row1, int row2, double alpha) {
+            for (var i = 0; i < matrix.ColsNum; i++) {
+                matrix[row2, i] += matrix[row1, i] * alpha;
+            }
+        }
+
+        private static IList<double> Foo(Matrix matrix) {
+            var result = new List<double>();
+            for (var i = matrix.RowsNum - 1; i >= 0; i--) {
+                var value = matrix[i, matrix.ColsNum - 1];
+                var k = 0;
+                for (var j = i + 1; j < matrix.ColsNum - 1; j++) {
+                    value -= matrix[i, j] * (result.Count > k ? result[k++] : 1);
+                }
+                
+                result.Insert(0, value / matrix[i, i]);
+            }
+
+            return result;
+        }
+
+        private static void ShowResult(IList<double> list, bool format = false) {
+            for (var i = 0; i < list.Count; i++) {
+                var value = format ? list[i].ToString("0.##") : list[i].ToString(CultureInfo.CurrentCulture);
+                Console.WriteLine($"x{i+1} = {value}");
+            }
+        }
+    }
+}
